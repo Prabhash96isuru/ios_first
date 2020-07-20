f9dc4b19e9fe256f36b0fd8ab10f50d5b2c6a5cf
isuru <67092667+Prabhash96isuru@users.noreply.github.com>
2020-07-20 09:07:35 -0700

    assignment one
    Assignment 1

---
+++ Assignment/Assignment_01/Assignment_01/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment_01
+//
+//  Created by Isuru Prabhash on 7/18/20.
+//  Copyright © 2020 Isuru Prabhash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios_first/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios_first
+//
+//  Created by Isuru Prabhash on 6/27/20.
+//  Copyright © 2020 Isuru Prabhash. All rights reserved.
+//	
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
